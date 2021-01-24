14fa986e8865a648ae932ec5f9bd2dd9c8566801
Jakob Fischer <jakob@automorph.info>
2021-01-24 16:30:18 +0100

    Preliminary solution for day 1

---
+++ day1/src/main.rs
@@ -0,0 +1,67 @@
+use std::collections::HashSet;
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+#[derive(PartialEq, Eq, Hash)]
+struct Accumulate {
+    number_of_parts: u8,
+    sum: i64,
+    product: i64,
+}
+
+#[allow(non_snake_case)]
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let f = File::open(&args[1]).unwrap();
+    let br = BufReader::new(f);
+
+    let mut visited = HashSet::<Accumulate>::new();
+    visited.insert(Accumulate {
+        number_of_parts: 0,
+        sum: 0,
+        product: 1,
+    });
+
+    for line in br.lines() {
+        if let Ok(val) = line.unwrap().parse::<i32>() {
+            let newVisited: HashSet<Accumulate> = visited
+                .iter()
+                .map(|x| Accumulate {
+                    number_of_parts: x.number_of_parts + 1,
+                    sum: x.sum + val as i64,
+                    product: x.product * val as i64,
+                })
+                .filter(|x| x.number_of_parts <= 3 && x.sum <= 2020)
+                .collect();
+
+            for toAdd in newVisited {
+                if toAdd.number_of_parts == 3 && toAdd.sum == 2020 {
+                    println!("result = {:?}", toAdd.product);
+                }
+                visited.insert(toAdd);
+            }
+        }
+    }
+
+    println!("visited = {:?}", visited.len());
+}
+
+/*fn main() {
+    let args: Vec<String> = env::args().collect();
+    let f = File::open(&args[1]).unwrap();
+    let br = BufReader::new(f);
+
+    let mut visited = HashSet::<i32>::new();
+
+    for line in br.lines() {
+        if let Ok(val) = line.unwrap().parse::<i32>() {
+            let complement = 2020 - val;
+            if visited.contains(&complement) {
+                println!("{:?}", complement*val);
+            }
+            visited.insert(val);
+        }
+    }
+}*/
